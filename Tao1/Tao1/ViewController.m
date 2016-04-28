5ed279dff0c24fcaacaadb1cc6a1a0e18697079e
lkjy <lkjy@shangdan1v2mac-mini.local>
2016-04-28 11:06:16 +0800

    hih

---
+++ Tao1/Tao1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Tao1
+//
+//  Created by lkjy on 16/4/28.
+//  Copyright © 2016年 张涛. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"wsafjf");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
